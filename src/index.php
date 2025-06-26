85f1762435b9ab02902a32da1cdf28e4314cef62
barondesmond <baron@desmond.com>
2025-06-26 15:54:11 -0500

    test

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<?php
+
+function nonRepeatingString(string $string): mixed
+{
+    $i = 0;
+    $nonRepeatingString = [];
+
+    while (isset($string[$i]) && strlen($string[$i]) > 0) {
+        if (!isset($nonRepeatingString[$string[$i]])) {
+            $nonRepeatingString[$string[$i]] = 0;
+        }
+        $nonRepeatingString[$string[$i]] = $nonRepeatingString[$string[$i]] + 1;
+        $i++;
+    }
+
+    foreach ($nonRepeatingString as $char => $int) {
+        if ($int === 1) {
+            return $char;
+        }
+    }
+
+    return -1;
+
+}
+
+echo nonRepeatingString("as the world a chruns");
